309053d97b65ca716979054f868ab10e3c69c151
salmamoh905 <samiramoh246@gmail.com>
2022-05-10 23:40:24 +0300

    added pagination using routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,30 @@
 
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
 
-function App() {
-  return (
-    <div className="App">
-      <h1>welcome POWERFUL WOMAN</h1>
-    </div>
-  );
+} from 'react-router-dom'
+import Home from './pages/home/Home'
+import List from './pages/list/List'
+import Hotel from './pages/hotel/Hotel'
+function App(){
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element ={<Home/>}/>
+      <Route path="/hotels" element ={<List/>}/>
+      <Route path="/hotels/:id" element ={<Hotel/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>welcome POWERFUL WOMAN</h1>
+//     </div>
+//   );
+// }
 
 export default App;
