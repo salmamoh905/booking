2e93cc2efecbe781df186d6eec736a0ecefd3be3
salmamoh905 <samiramoh246@gmail.com>
2022-05-16 08:33:02 +0300

    Completed Footer,Mail,hotel pages and Login using context

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,24 @@
-
 import {
   BrowserRouter,
   Routes,
-  Route
+  Route,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List";
+import Login from "./pages/login/Login";
 
-} from 'react-router-dom'
-import Home from './pages/home/Home'
-import List from './pages/list/List'
-import Hotel from './pages/hotel/Hotel'
-function App(){
-  return(
+function App() {
+  return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element ={<Home/>}/>
-      <Route path="/hotels" element ={<List/>}/>
-      <Route path="/hotels/:id" element ={<Hotel/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/hotels" element={<List/>}/>
+        <Route path="/hotels/:id" element={<Hotel/>}/>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>welcome POWERFUL WOMAN</h1>
-//     </div>
-//   );
-// }
 
 export default App;
